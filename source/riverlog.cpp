4b0e2f270c1d9fd5ce42c7bdacde467be74dadef
ASourLemon <nelsonflsilva@gmail.com>
2014-10-12 23:25:47 +0100

    Added color to map and objects.

--- source/riverlog.cpp
+++ source/riverlog.cpp
@@ -16,9 +16,9 @@ Riverlog::Riverlog(float x, float y, float z, int quality) {
 	body.createCylinder(LOG_HEIGHT, LOG_RADIOS, quality);
 
 	body.setMaterialBlockName("Materials");
-	float f[4] = {0.8f, 0.6f, 0.4f, 1.0f};
+	float f[4] = {0.4f, 0.2f, 0.0f, 1.0f};
 	body.setColor(VSResourceLib::DIFFUSE, f);
-	float f2[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+	float f2[4] = {0.4f, 0.2f, 0.0f, 1.0f};
 	body.setColor(VSResourceLib::AMBIENT, f2);
 
 }
