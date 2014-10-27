24b5336f62455e99b9b52f42407cdbebc1086206
ASourLemon <nelsonflsilva@gmail.com>
2014-10-27 22:23:51 +0000

    Refactored map

--- include/map.h
+++ include/map.h
@@ -5,7 +5,6 @@
 
 #define MAP0_W 20
 #define MAP0_H 32
-#define WALL_CHAR 'X'
 #define WATER_SPEED 0.3f
 
 namespace domain {
@@ -14,6 +13,27 @@ namespace domain {
 
 class Map : public Drawable {
 private:
+	static float roadAmb[4];
+	static float roadDif[4];
+	static float roadSpec[4];
+	static float roadShininess;
+
+	static float grassAmb[4];
+	static float grassDif[4];
+	static float grassSpec[4];
+	static float grassShininess;
+
+	static float waterAmb[4];
+	static float waterDif[4];
+	static float waterSpec[4];
+	static float waterShininess;	
+	
+	static float wallAmb[4];
+	static float wallDif[4];
+	static float wallSpec[4];
+	static float wallShininess;
+	
+protected:
 	VSResSurfRevLib body;
 	VSResSurfRevLib water;
 	float deltaWater;
--- source/map.cpp
+++ source/map.cpp
@@ -3,13 +3,33 @@
 extern GLuint TextureArray[3];
 
 namespace domain {
+	
+float Map::roadAmb[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
+float Map::roadDif[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
+float Map::roadSpec[4] = { 0.6f, 0.6f, 0.6f, 1.0f };
+float Map::roadShininess = 100;
+
+float Map::grassAmb[4] = { 0.02f, 0.05f, 0.02f, 1.0f };
+float Map::grassDif[4] = { 0.2f, 0.5f, 0.2f, 1.0f };
+float Map::grassSpec[4] = { 0.4f, 0.4f, 0.4f, 1.0f };
+float Map::grassShininess = 100;
+
+float Map::waterAmb[4] = { 0.2f, 0.2f, 0.5f, 1.0f };
+float Map::waterDif[4] = { 0.4f, 0.6f, 0.4f, 1.0f };
+float Map::waterSpec[4] = { 0.7f, 0.7f, 0.7f, 1.0f };
+float Map::waterShininess = 100;
+
+float Map::wallAmb[4] = { 0.8f, 0.6f, 0.4f, 1.0f };
+float Map::wallDif[4] = { 0.2f, 0.2f, 0.75f, 1.0f };
+float Map::wallSpec[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+float Map::wallShininess = 1;
 
 Map::Map() {
 
 	body.createCube(1.0f);
 	water.createSphere(2.4, 10);
 
-	deltaWater = 0;
+	deltaWater = 0.0f;
 	goingLeft = false;
 
 }
@@ -20,12 +40,10 @@ Map::~Map() {
 void Map::draw(VSMathLib* core) {
 
 	body.setMaterialBlockName("Materials");
-	float d1[4] = { 0.8f, 0.6f, 0.4f, 1.0f };
-	body.setColor(VSResourceLib::DIFFUSE, d1);
-	float f2[4] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	body.setColor(VSResourceLib::AMBIENT, f2);
-	float rawr[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
-	body.setColor(VSResourceLib::SPECULAR, rawr);
+	body.setColor(VSResourceLib::DIFFUSE, wallDif);
+	body.setColor(VSResourceLib::AMBIENT, wallAmb);
+	body.setColor(VSResourceLib::SPECULAR, wallSpec);
+	body.setColor(VSResourceLib::SHININESS, &wallShininess);
 
 	////////////////////////////////////
 	/////////////////////////////////// walls
@@ -33,21 +51,57 @@ void Map::draw(VSMathLib* core) {
 	//top
 	core->pushMatrix(VSMathLib::MODEL);
 	core->translate(0.0, -1.0, -1.0);
-	core->scale(MAP0_W, 2.0, 1.0);
+	core->scale(MAP0_W, 3.5, 1.0);
 	body.render();
 	core->popMatrix(VSMathLib::MODEL);
 
 	//right
 	core->pushMatrix(VSMathLib::MODEL);
 	core->translate(MAP0_W, -1.0, 0.0);
-	core->scale(1.0, 2.0, MAP0_H);
+	core->scale(1.0, 3.5, 3.0);
+	body.render();
+	core->popMatrix(VSMathLib::MODEL);
+
+	core->pushMatrix(VSMathLib::MODEL);
+	core->translate(MAP0_W, 1.5, 3.0);
+	core->scale(1.0, 1.0, 10.0);
+	body.render();
+	core->popMatrix(VSMathLib::MODEL);
+
+	core->pushMatrix(VSMathLib::MODEL);
+	core->translate(MAP0_W, -1.0, 13.0);
+	core->scale(1.0, 3.5, 3.0);
+	body.render();
+	core->popMatrix(VSMathLib::MODEL);
+
+	core->pushMatrix(VSMathLib::MODEL);
+	core->translate(MAP0_W, -1.0, 24.0);
+	core->scale(1.0, 3.0, 8.0);
 	body.render();
 	core->popMatrix(VSMathLib::MODEL);
 
 	//left
 	core->pushMatrix(VSMathLib::MODEL);
 	core->translate(-1.0, -1.0, 0.0);
-	core->scale(1.0, 2.0, MAP0_H);
+	core->scale(1.0, 3.5, 3.0);
+	body.render();
+	core->popMatrix(VSMathLib::MODEL);
+
+	core->pushMatrix(VSMathLib::MODEL);
+	core->translate(-1.0, 1.5, 3.0);
+	core->scale(1.0, 1.0, 10.0);
+	body.render();
+	core->popMatrix(VSMathLib::MODEL);
+
+	core->pushMatrix(VSMathLib::MODEL);
+	core->translate(-1.0, -1.0, 13.0);
+	core->scale(1.0, 3.5, 3.0);
+	body.render();
+	core->popMatrix(VSMathLib::MODEL);
+
+	core->pushMatrix(VSMathLib::MODEL);
+	core->translate(-1.0, -1.0, 24.0);
+	core->scale(1.0, 3.0, 8.0);
 	body.render();
 	core->popMatrix(VSMathLib::MODEL);
 
@@ -58,13 +112,15 @@ void Map::draw(VSMathLib* core) {
 	body.render();
 	core->popMatrix(VSMathLib::MODEL);
 
-	float d2[4] = { 0.2f, 0.5f, 0.2f, 1.0f };
-	body.setColor(VSResourceLib::DIFFUSE, d2);
-	float f3[4] = { 0.02f, 0.05f, 0.02f, 1.0f };
-	body.setColor(VSResourceLib::AMBIENT, f3);
 	///////////////////////////////////
 	/////////////////////////////////// floor
 	///////////////////////////////////
+
+	body.setColor(VSResourceLib::DIFFUSE, grassDif);
+	body.setColor(VSResourceLib::AMBIENT, grassAmb);
+	body.setColor(VSResourceLib::SPECULAR, grassSpec);
+	body.setColor(VSResourceLib::SHININESS, &grassShininess);
+
 	//top border
 	core->pushMatrix(VSMathLib::MODEL);
 	core->translate(0.0, -1.0, 0.0);
@@ -86,30 +142,38 @@ void Map::draw(VSMathLib* core) {
 	body.render();
 	core->popMatrix(VSMathLib::MODEL);
 
-	float d3[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
-	body.setColor(VSResourceLib::DIFFUSE, d3);
-	float f5[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
-	body.setColor(VSResourceLib::AMBIENT, f5);
-	//road	FIXME:	CHANGE MATERIAL
+
+	/////////////////////////////////////////////////
+	///ROAD/////////////////////////////////////////
+	////////////////////////////////////////////////
+
+	body.setColor(VSResourceLib::DIFFUSE,roadDif);
+	body.setColor(VSResourceLib::AMBIENT, roadAmb);
+	body.setColor(VSResourceLib::SPECULAR, roadSpec);
+	body.setColor(VSResourceLib::SHININESS, &roadShininess);
+
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, TextureArray[1]);
 
 	core->pushMatrix(VSMathLib::MODEL);
 	core->translate(0.0, -1.0, 3.0);
-	core->scale(MAP0_W, 1, 10);
+	core->scale(MAP0_W+4, 1, 10);
 	body.render();
 	core->popMatrix(VSMathLib::MODEL);
+
+
+	/////////////////////////////////////////////////
+	///RIVER/////////////////////////////////////////
+	////////////////////////////////////////////////
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, TextureArray[0]);
 
 	water.setMaterialBlockName("Materials");
-	float d4[4] = { 0.2f, 0.2f, 0.75f, 1.0f };
-	water.setColor(VSResourceLib::DIFFUSE, d4);
-	float f6[4] = { 0.2f, 0.2f, 0.5f, 1.0f };
-	water.setColor(VSResourceLib::AMBIENT, f6);
-
-	//river	FIXME:	CHANGE MATERIAL
+	water.setColor(VSResourceLib::DIFFUSE, waterDif);
+	water.setColor(VSResourceLib::AMBIENT, waterAmb);
+	water.setColor(VSResourceLib::SPECULAR, waterSpec);
+	water.setColor(VSResourceLib::SHININESS, &waterShininess);
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, TextureArray[2]);
@@ -117,7 +181,7 @@ void Map::draw(VSMathLib* core) {
 	core->pushMatrix(VSMathLib::MODEL);
 	core->translate(0.0, -2.0, 16.0);	//base translate
 	for (int l = 0.5; l < 10; l += 3) {
-		for (int c = 0; c <= MAP0_W; c += 2) {
+		for (int c = -2; c <= MAP0_W+2; c += 2) {
 			core->pushMatrix(VSMathLib::MODEL);
 			if (l % 2 == 0) {
 				core->translate(c + deltaWater, -1.0, l);
@@ -147,4 +211,4 @@ void Map::tick() {
 
 }
 
-} /* namespace domain */
+}
