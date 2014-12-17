2faf1fd17968bb8651de082cc7e3380a04d20133
Joao Miranda <joaoshmiranda@hotmail.com>
2014-12-17 06:32:59 +0000

    3 angles ok but still shaky...

--- WebGL/source/FrogPerspectiveCamera.js
+++ WebGL/source/FrogPerspectiveCamera.js
@@ -1,4 +1,5 @@
 var alpha, beta, gamma;
+var prev_alpha, prev_beta, prev_gamma;
 
 function FrogPerspectiveCamera(){
 	this.fov = 70.0;
@@ -7,16 +8,30 @@ function FrogPerspectiveCamera(){
 	this.far = 40.1;
 
 	this.pos = [1.0, 1.0, 0.5];
-	this.up = [0.0, 0.0, 1.0];
+	this.up = [0.0, 3.0, 6.5];
 	this.at = [0.0, 2.0, 0.0];
 }
 
 FrogPerspectiveCamera.prototype.load = function() {
+    ////////////// Rotate over yy axis ///////////////
+	var mat_gamma = mat4.create();
+	var up_vec3 = vec3.clone(this.up);
+	var up_vec4;
+
+	mat4.rotate(mat_gamma, mat_gamma, gamma ? gamma*Math.PI/180 : 0, [0.0, 1.0, 0.0]);
+	mat4.rotate(mat_gamma, mat_gamma, beta ? beta*Math.PI/180 : 0, [1.0, 0.0, 0.0]);
+	up_vec4 = vec4.fromValues(up_vec3[0], up_vec3[1], up_vec3[2], 0.0);
+	vec4.transformMat4(up_vec4, up_vec4, mat_gamma);
+	up_vec3 = vec3.fromValues(up_vec4[0], up_vec4[1], up_vec4[2]);
+	///////////////////////////////////////////////////
+
+
 	////////////// Rotate over zz axis //////////////
 	var mat_alpha = mat4.create();
 	var at_vec3 = vec3.create();
 	var at_vec4;
 
+	mat4.rotate(mat_alpha, mat_alpha, beta ? beta*Math.PI/180 : 0, [1.0, 0.0, 0.0]);
 	mat4.rotate(mat_alpha, mat_alpha, alpha ? alpha*Math.PI/180 : 0, [0.0, 0.0, 1.0]);
 	vec3.sub(at_vec3, this.at, this.pos);
 	at_vec4 = vec4.fromValues(at_vec3[0], at_vec3[1], at_vec3[2], 0.0);
@@ -24,18 +39,6 @@ FrogPerspectiveCamera.prototype.load = function() {
 	at_vec3 = vec3.fromValues(at_vec4[0], at_vec4[1], at_vec4[2]);
 	vec3.add(at_vec3, at_vec3, this.pos);
     //////////////////////////////////////////////////
-
-
-    ////////////// Rotate over yy axis ///////////////
-	var mat_gamma = mat4.create();
-	var up_vec3 = vec3.clone(this.up);
-	var up_vec4;
-
-	mat4.rotate(mat_gamma, mat_gamma, gamma ? gamma*Math.PI/180 : 0, [0.0, 1.0, 0.0]);
-	up_vec4 = vec4.fromValues(up_vec3[0], up_vec3[1], up_vec3[2], 0.0);
-	vec4.transformMat4(up_vec4, up_vec4, mat_gamma);
-	up_vec3 = vec3.fromValues(up_vec4[0], up_vec4[1], up_vec4[2]);
-	///////////////////////////////////////////////////
 
 
 	mat4.perspective(pMatrix, this.fov, this.aspectRatio, this.near, this.far);
@@ -53,10 +56,30 @@ FrogPerspectiveCamera.prototype.update = function() {
 }
 
 // Create an event handler function for processing the device orientation event
+var isInit = false;
 FrogPerspectiveCamera.prototype.handleOrientationEvent = function(e) {
+	var TOLERANCE = 10;
     alpha = e.alpha;
     beta = e.beta;
     gamma = e.gamma;
+
+    if(isInit) {
+    	if(prev_alpha - alpha <= -TOLERANCE || prev_alpha - alpha >= TOLERANCE)
+    		prev_alpha = alpha;
+    	if(prev_beta - beta <= -TOLERANCE || prev_beta - beta >= TOLERANCE)
+    		prev_beta = beta;
+    	if(prev_gamma - gamma <= -TOLERANCE || prev_gamma - gamma >= TOLERANCE)
+    		prev_gamma = gamma;
+    } else {
+    	isInit = true;
+		prev_alpha = alpha;
+		prev_beta = beta;
+		prev_gamma = gamma;
+    }
+
+	alpha = prev_alpha;
+	beta = prev_beta;
+	gamma = prev_gamma;
 
     //////// FOR DEBUG ///////
     //var text = gamma.toString();
